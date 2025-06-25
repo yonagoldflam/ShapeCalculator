5c99c3db70d3b3672177d5834d4345bab01d18e7
tzirkus <zalmen5779@gmail.com>
2025-06-25 12:50:57 +0300

    add triangle

--- menu.py
+++ menu.py
@@ -5,7 +5,7 @@ def menu():
     calculator = Calculator()
     out = False
     while not out:
-        choose = input("1.disply all shapes \n2. create rectangle \n3. create square \n")
+        choose = input("1.disply all shapes \n2. create rectangle \n3. create square \n4. create triangle \n")
         match choose:
             case '1':
                 calculator.disply_shaps()
@@ -13,6 +13,10 @@ def menu():
                 calculator.add_shape("rectangle")
             case '3':
                 calculator.add_shape("square")
+            case '4':
+                calculator.add_shape('triangle')
+
+
             case _:
                 out =True
 
